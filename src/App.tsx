4aefbe65597b6699c4ecb001a355217b166a0e4a
Diego Ávalos <avalosdiego95@gmail.com>
2024-10-04 20:11:29 -0600

    Render NavBar and InteractiveMap components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import InteractiveMap from "./components/InteractiveMap";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-    <h1>El dago y el gacho</h1>
-    </div>
-  )
+    <>
+      <NavBar />
+      <InteractiveMap />
+    </>
+  );
 }
 
-export default App
+export default App;
