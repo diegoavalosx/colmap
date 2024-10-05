06c1743121198fe52b83e5213e5f0d7e01cf083a
Diego Ávalos <avalosdiego95@gmail.com>
2024-10-04 19:31:19 -0600

    Create InteractiveMap component and add basic functionality

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -1,5 +1,53 @@
+import {
+  APIProvider,
+  // biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+import { useState } from "react";
+
 const InteractiveMap = () => {
-  return <></>;
+  const [open, setOpen] = useState(false);
+  const position = { lat: 19.2488939, lng: -103.7521401 };
+  const daisushiPosition = { lat: 19.2654075, lng: -103.7391145 };
+  const daisushiPinoPosition = { lat: 19.2529951, lng: -103.7331614 };
+
+  return (
+    <APIProvider apiKey="AIzaSyA0PZVMqFew4mwbrAfOHs1muylqfyGYqn8">
+      <div style={{ height: "100%", width: "100%" }}>
+        <Map
+          zoom={14}
+          center={position}
+          mapId="80b9549366c22aeb"
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+        >
+          <AdvancedMarker
+            position={daisushiPosition}
+            onClick={() => setOpen(true)}
+          >
+            <Pin background="white" />
+          </AdvancedMarker>
+          <AdvancedMarker
+            position={daisushiPinoPosition}
+            onClick={() => setOpen(true)}
+          >
+            <Pin background="white" />
+          </AdvancedMarker>
+        </Map>
+        {open ? (
+          <InfoWindow
+            position={daisushiPosition}
+            onCloseClick={() => setOpen(false)}
+          >
+            <p>Aquí está Daisushii</p>
+          </InfoWindow>
+        ) : null}
+      </div>
+    </APIProvider>
+  );
 };
 
 export default InteractiveMap;
