c564575c42382bbab6d42eb542cd8dc6f6181280
HiramDev <hiramdev02@gmail.com>
2024-10-04 19:32:24 -0600

    Agregué el componente NavBar

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Rutas de los archivos donde utilizarás Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
