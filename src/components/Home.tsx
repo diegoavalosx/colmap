6593b40e715f6f85ce958eb6535e27475c9edc96
HiramDev <hiramdev02@gmail.com>
2024-10-16 18:34:24 -0600

    Add Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    return (
+        <div className="mt-10  ms-5 font-semibold text-wrap">
+            <h1>Home</h1>
+            <div style={{ height: '1000px', backgroundColor: '#92A8D1'}}></div>
+      </div>
+    );
+}
+
+export default Home;
