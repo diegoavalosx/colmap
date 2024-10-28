e9fccd8cb229bbc1bcf6c1c1c2444373300bef8a
HiramDev <hiramdev02@gmail.com>
2024-10-28 17:24:59 -0600

    Agregando cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.css";
 import InteractiveMap from "./components/InteractiveMap";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home"
+import React, { useRef } from "react";
 
 function App() {
+  const links = [
+    {id: 1, name: 'Home', ref: useRef()},
+    {id: 2, name: 'Services', ref: useRef()},
+    {id: 3, name: 'About', ref: useRef()},
+    {id: 4, name: 'Contact', ref: useRef()},
+  ]
+
   return (
-    <>
-      <NavBar />
+    <React.Fragment>
+      <NavBar links={links} />
       <InteractiveMap />
-      
-    </>
+      <Home ref={links[0].ref} />
+    </React.Fragment>
   );
 }
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,11 @@
-
-
 const Home = () => {
+    const homeRef = useRef<HTMLDivElement | null>(null);
+
     return (
-        <div className="mt-10  ms-5 font-semibold text-wrap">
-            <h1>Home</h1>
-            <div style={{ height: '1000px', backgroundColor: '#92A8D1'}}></div>
+        <div ref={homeRef} className="mt-10  ms-5 font-semibold text-wrap">
+            <h1>Home
+            </h1>
+            <div className="h-[1000px] bg-[#92A8D1]"></div>
       </div>
     );
 }
