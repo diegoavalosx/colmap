ca830bfc3489cacfd035e62ba4392feb8bd6c997
Diego Ávalos <avalosdiego95@gmail.com>
2024-11-18 11:56:59 -0600

    Fetch locations from database depending on logged in user

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -7,9 +7,7 @@ const MainContent: React.FC = () => {
   return (
     <>
       {user && (
-        <h1 className="text-3xl font-semibold mb-4">
-          Welcome to the Dashboard
-        </h1>
+        <h1 className="text-3xl font-semibold">Welcome to the Dashboard</h1>
       )}
       {/*<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className="bg-white p-4 shadow rounded">Widget 1</div>
