b2750088c0155d302f9214796cbca2b34b9e569d
Diego Ávalos <diegoavalosx@gmail.com>
2024-11-29 00:14:40 -0600

    New version is now responsive

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -12,19 +12,21 @@ const WhoWeAre = forwardRef<HTMLDivElement, WhoWeAreProps>(
       <div
         id={id}
         ref={ref}
-        className="h-screen bg-deluxe-black text-white md:p-16 grid grid-cols-1 md:grid-cols-2 gap-12 animate-fadeIn"
+        className="min-h-screen h-auto p-6 bg-deluxe-black text-white md:p-16 grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 animate-fadeIn"
       >
         {goBackToHome ? (
           <button
             type="button"
-            className="text-2xl font-bold mb-4 absolute top-20 left-20 hover:scale-110 hover:text-ooh-yeah-pink transition-colors duration-300"
+            className="text-lg md:text-3xl font-bold mb-4 absolute top-7 left-7 md:top-20 md:left-20 hover:scale-110 hover:text-ooh-yeah-pink transition-colors duration-300"
             onClick={() => goBackToHome()}
           >
-            GO BACK TO HOME
+            BACK
           </button>
         ) : null}
-        <div className="space-y-6 flex flex-col justify-center w-full h-full">
-          <h1 className="text-4xl font-bold mb-4">WHO WE ARE</h1>
+        <div className="space-y-6 flex flex-col justify-center w-full h-full self-stretch">
+          <h1 className="text-2xl md:text-4xl font-bold mb-0 md:mb-4 text-center md:text-left">
+            WHO WE ARE
+          </h1>
           <p className="text-lg font-light leading-relaxed">
             Oohyeah is a boutique outdoor media agency specializing in{" "}
             <span className="text-ooh-yeah-pink font-medium">distinctive</span>{" "}
--- src/components/WhyUs.tsx
+++ src/components/WhyUs.tsx
@@ -35,23 +35,25 @@ const features = [
 const WhyUs = forwardRef<HTMLDivElement, WhyUsProps>(
   ({ id, goBackToHome }, ref) => {
     return (
-      <div className="h-screen bg-deluxe-gray">
+      <div className="h-auto min-h-screen flex flex-col bg-deluxe-gray">
         <div
           id={id}
           ref={ref}
-          className="h-full bg-deluxe-black text-white p-8 md:p-16 grid grid-cols-1 md:grid-cols-2 gap-12 items-center animate-fadeIn rounded-bl-custom-br "
+          className="h-full flex-grow bg-deluxe-black text-white p-6 md:p-16 grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 items-center animate-fadeIn rounded-none md:rounded-bl-custom-br "
         >
           {goBackToHome ? (
             <button
               type="button"
-              className="text-2xl font-bold mb-4 absolute top-20 left-20 hover:scale-110 hover:text-ooh-yeah-pink transition-colors duration-300"
+              className="text-lg md:text-3xl font-bold mb-4 absolute top-7 left-7 md:top-20 md:left-20 hover:scale-110 hover:text-ooh-yeah-pink transition-colors duration-300"
               onClick={() => goBackToHome()}
             >
-              GO BACK TO HOME
+              BACK
             </button>
           ) : null}
           <div className="space-y-6 flex flex-col justify-center w-full h-full">
-            <h1 className="text-3xl md:text-4xl font-bold">WHY US</h1>
+            <h1 className="text-2xl md:text-4xl font-bold mb-0 md:mb-4 text-center md:text-left">
+              WHY US
+            </h1>
             <div className="space-y-4">
               <p className="text-lg font-light leading-relaxed">
                 Wild posting creates an urban appeal that captivates audiences
@@ -65,16 +67,18 @@ const WhyUs = forwardRef<HTMLDivElement, WhyUsProps>(
               </p>
             </div>
           </div>
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          <div className="grid grid-cols-2 gap-2 md:gap-6 grid-rows-2 md:grid-rows-1">
             {features.map((feature) => (
               <div
                 key={feature.id}
                 className="p-6 bg-gray-800 rounded-lg shadow-lg hover:scale-105 transition-transform"
               >
-                <h3 className="text-xl font-semibold mb-2 flex items-start space-x-3">
+                <h3 className="text-md md:text-xl font-semibold flex items-start space-x-3">
                   {feature.title}
                 </h3>
-                <p className="text-sm font-light">{feature.text}</p>
+                <p className="hidden md:block text-sm font-light">
+                  {feature.text}
+                </p>
               </div>
             ))}
           </div>
