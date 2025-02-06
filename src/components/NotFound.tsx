3d6a287650d47750d47dd7c172f28bb844a53ed4
HiramDev <hiramdev02@gmail.com>
2025-02-05 19:19:23 -0600

    delete question mark

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white p-4">
         <div className="text-center max-w-2xl">
             <h1 className="text-6xl md:text-9xl font-bold text-ooh-yeah-pink mb-4">404</h1>
-            <h2 className="text-2xl md:text-4xl font-semibold mb-4">¡Page not found!</h2>
+            <h2 className="text-2xl md:text-4xl font-semibold mb-4">Page not found!</h2>
             <p className="text-base md:text-lg text-gray-300 mb-8">We're sorry, the page you're looking for does not exist</p>
             <a
               href="/v3"
